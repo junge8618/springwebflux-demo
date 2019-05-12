340a021f8c9151517768d37f29a9bcc5d3dea337
junge8618 <513851613@qq.com>
2019-05-12 08:33:35 +0800

    项目初始化

---
+++ springwebflux-helloworld/src/main/java/com/junge/demo/springwebfluxhelloworld/SpringwebfluxHelloworldApplication.java
@@ -0,0 +1,13 @@
+package com.junge.demo.springwebfluxhelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringwebfluxHelloworldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringwebfluxHelloworldApplication.class, args);
+    }
+
+}
